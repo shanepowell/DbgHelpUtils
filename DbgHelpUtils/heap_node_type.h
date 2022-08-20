95cd7a5a5fe51804c5f4434f7548bf4593d1d4ac
Shane Powell <killerbee@club88.co.nz>
2022-08-21 10:38:06 +1200

    Add heap_node_type enum

---
+++ DbgHelpUtils/heap_node_type.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+namespace dlg_help_utils::heap
+{
+    enum class heap_node_type
+    {
+        nt_heap_lfh_entry,
+        nt_heap_segment_entry,
+        nt_heap_virtual_entry,
+        segment_backend_entry,
+        segment_entry,
+        segment_lfh_entry,
+        segment_large_entry,
+        dph_entry,
+        dph_virtual_entry,
+        memory_range
+    };
+}
